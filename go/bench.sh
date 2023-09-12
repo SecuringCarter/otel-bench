8431d091349fa35afff6dfccb3024bdae03ded24
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2023-09-12 10:47:13 -0500

    Fix go job

--- go/bench.sh
+++ go/bench.sh
@@ -5,7 +5,7 @@ REPO="${REPO:-opentelemetry-go}"
 
 if [ ! -e "${REPO}/go.mod" ]; then 
     ## clone otel-go main
-    git clone --depth 1 --branch main https://github.com/open-telemetry/${REPO}.git 
+    git clone --depth 1 --branch main "https://github.com/open-telemetry/${REPO}.git" 
 fi
 
 mkdir bench-output 2>/dev/null || true
@@ -13,8 +13,13 @@ mkdir bench-output 2>/dev/null || true
 if [ -e "bench-output/${REPO}.txt" ]; then 
     rm "bench-output/${REPO}.txt"
 fi
-for dir in $(grep -r "func Bench" ${REPO}/ -l | sed -s 's#/[a-z0-9\._]*\.go$##' | sort | uniq); do
+for dir in $(grep -r "func Bench" "${REPO}/" -l | sed -s 's#/[a-z0-9\._]*\.go$##' | sort | uniq); do
     echo -n "$dir "
-    #go -C "${dir}" test -bench ./... -run=^$ >> "bench-output/${REPO}.txt"
+    if [ -z "$VERBOSE" ]; then
+        go -C "${dir}" test -bench ./... -run=^$ >> "bench-output/${REPO}.txt"
+    else
+        echo ""
+        go -C "${dir}" test -bench ./... -run=^$ | tee -a "bench-output/${REPO}.txt"
+    fi
     echo "- done"
 done
