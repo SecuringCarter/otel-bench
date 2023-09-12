f41fe79c15ee32c4c32431e86aec73eefd710d64
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2023-09-12 09:55:32 -0500

    add simple go benchmark tool

---
+++ go/bench.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+REPO="${REPO:-opentelemetry-go}"
+
+
+if [ ! -e "${REPO}/go.mod" ]; then 
+    ## clone otel-go main
+    git clone --depth 1 --branch main https://github.com/open-telemetry/${REPO}.git 
+fi
+
+mkdir bench-output 2>/dev/null || true
+
+if [ -e "bench-output/${REPO}.txt" ]; then 
+    rm "bench-output/${REPO}.txt"
+fi
+for dir in $(grep -r "func Bench" ${REPO}/ -l | sed -s 's#/[a-z_]*\.go##' | sort | uniq); do
+    echo -n "$dir "
+    go -C "${dir}" test -bench ./... -run=^$ >> "bench-output/${REPO}.txt"
+    echo "- done"
+done
